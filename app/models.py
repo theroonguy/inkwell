e396d5f4586ae87ee02975a886138b5ba47153eb
Haroon <haroon.a.amanullah@gmail.com>
2023-03-28 21:49:36 -0400

    logins work!

--- app/models.py
+++ app/models.py
@@ -1,25 +1,37 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    user_firtname = db.Column(db.String(64), index=True)
-    user_lastname = db.Column(db.String(64), index=True)
-    user_email = db.Column(db.String(120), index=True, unique=True)
-    user_password = db.Column(db.String(128))
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
     books = db.relationship('Book', backref='author', lazy='dynamic')
 
     def __repr__(self): # __repr__ tells python how to print objects of this class for debugging
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def get_id(self):
+        return str(self.user_id)
+
 class Book(db.Model):
     book_id = db.Column(db.Integer, primary_key=True)
     book_title = db.Column(db.String(64))
-    book_synopsis = db.Column(db.String(64))
     content = db.Column(db.String(64))
     date_uploaded = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
     def __repr__(self):
         return '<Book {}>'.format(self.content)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,35 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from app.models import Book
+from app.models import Book, User
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Haroon and Ayman'}
     books = Book.query.all()
-    return render_template('index.html', title='Home', user=user, books=books)
+    return render_template('index.html', title='Home', books=books)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:   # if user is logged in, direct them out of this page
         return redirect(url_for('index'))
+    form = LoginForm() 
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
