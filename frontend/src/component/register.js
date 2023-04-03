83eb3a5b8f48522c39fce19f620c8c68265cb49f
Haroon <haroon.a.amanullah@gmail.com>
2023-04-03 13:39:53 -0500

    stuff

--- frontend/src/component/register.js
+++ frontend/src/component/register.js
@@ -1,24 +1,46 @@
 import React, { useState } from 'react'
 import { Form, Button, FormGroup } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 const RegisterPage = () => {
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
 
-    const submitForm = () => {
-        console.log("Form Submitted");
-        console.log(username)
-        console.log(email)
-        console.log(password)
-        console.log(confirmPassword)
+    const { register, watch, handleSubmit, reset, formState: { errors } } = useForm();
 
-        setUsername('')
-        setEmail('')
-        setPassword('')
-        setConfirmPassword('')
+    const submitForm = (data) => {
+
+        if (data.password === data.confirmPassword) {
+
+            const body = {
+                username: data.username,
+                email: data.email,
+                password: data.password
+            }
+
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            }
+
+            fetch('/auth/register', requestOptions)
+            .then(res=>res.json())
+            .then(data=>console.log(data))
+            .catch(err=>console.log(err))
+
+            reset()
+        } else {
+            alert("Passwords do not match.")
+        }
+
     }
+
+    console.log(watch("username"));
+    console.log(watch("email"));
+    console.log(watch("password"));
+    console.log(watch("confirmPassword"));
 
     return (
         <div>
@@ -29,41 +51,54 @@ const RegisterPage = () => {
                         <Form.Label>Username</Form.Label>
                         <Form.Control type="text"
                             placeholder="A unique username."
-                            value={username}
-                            name="username"
-                            onChange={(e) => { setUsername(e.target.value) }} />
+                            {...register("username", { required: true, maxLength: 25 })}
+                        />
+
+                        {errors.username && <p style={{ color: "red" }}><small>Username is required</small></p>}
+                        {errors.username?.type === "maxLength" && <p style={{ color: "red" }}><small>Max characters: 25</small></p>}
                     </Form.Group>
                     <br></br>
                     <Form.Group>
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email"
                             placeholder="A valid email address."
-                            value={email}
-                            name="email"
-                            onChange={(e) => { setEmail(e.target.value) }} />
+                            {...register("email", { required: true, maxLength: 80 })}
+                        />
+
+                        {errors.email && <p style={{ color: "red" }}><small>Email is required</small></p>}
+                        {errors.email?.type === "maxLength" && <p style={{ color: "red" }}><small>Max characters: 80</small></p>}
+
                     </Form.Group>
                     <br></br>
                     <Form.Group>
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password"
                             placeholder="Your password."
-                            value={password}
-                            name="password"
-                            onChange={(e) => { setPassword(e.target.value) }} />
+                            {...register("password", { required: true, minLength: 8 })}
+                        />
+
+                        {errors.password && <p style={{ color: "red" }}><small>Password is required</small></p>}
+                        {errors.password?.type === "minLength" && <p style={{ color: "red" }}><small>Min characters: 8</small></p>}
+
                     </Form.Group>
                     <br></br>
                     <Form.Group>
                         <Form.Label>Confirm Password</Form.Label>
                         <Form.Control type="password"
                             placeholder="Your password."
-                            value={confirmPassword}
-                            name="confirmPassword"
-                            onChange={(e) => { setConfirmPassword(e.target.value) }} />
+                            {...register("confirmPassword", { required: true, minLength: 8 })}
+                        />
+
+                        {errors.confirmPassword && <p style={{ color: "red" }}><small>Password confirmation is required</small></p>}
+                        {errors.confirmPassword?.type === "minLength" && <p style={{ color: "red" }}><small>Min characters: 8</small></p>}
+
                     </Form.Group>
                     <br></br>
-
                     <Form.Group>
-                        <Button onClick={submitForm}>Register</Button>
+                        <Button onClick={handleSubmit(submitForm)}>Register</Button>
+                    </Form.Group>
+                    <Form.Group>
+                        <small>Already have an account? <Link to="/login">Log in</Link></small>
                     </Form.Group>
                 </form>
             </div>
