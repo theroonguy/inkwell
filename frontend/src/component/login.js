6e98fc145af96362ba3e84ea726ae870e2330422
Ayman-Mahfuz <111789879+Ayman-Mahfuz@users.noreply.github.com>
2023-04-05 16:44:26 -0500

    login page styled! but its nothing crazy

--- frontend/src/component/login.js
+++ frontend/src/component/login.js
@@ -1,54 +1,59 @@
-import React, { useState, useEffect } from "react";
-import { Form, Button, FormGroup } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const submitForm = () => {
-        console.log("Form Submitted");
-        console.log(username)
-        console.log(password)
+  const submitForm = () => {
+    console.log("Form Submitted");
+    console.log(username);
+    console.log(password);
 
-        setUsername('')
-        setPassword('')
-    }
+    setUsername("");
+    setPassword("");
+  };
 
-    return (
-        <div className="login-page">
-            <div className="form-container">
-                <h1>Log In</h1>
-                <form>
-                    <Form.Group>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text"
-                            placeholder="A unique username."
-                            value={username}
-                            name="username"
-                            onChange={(e) => { setUsername(e.target.value) }} />
-                    </Form.Group>
-                    <br></br>
-                    <Form.Group>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password"
-                            placeholder="Your password."
-                            value={password}
-                            name="password"
-                            onChange={(e) => { setPassword(e.target.value) }} />
-                    </Form.Group>
-                    <br></br>
-
-                    <Form.Group>
-                        <Button onClick={submitForm}>Log In</Button>
-                    </Form.Group>
-                    <Form.Group>
-                        <small>Don't have an account? <Link to="/register">Register</Link></small>
-                    </Form.Group>
-                </form>
-            </div>
-        </div>
-    )
+  return (
+    <div className="login-page">
+      <div className="form-container">
+        <h1>Log In</h1>
+        <Form>
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="A unique username"
+              value={username}
+              name="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Your password"
+              value={password}
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <div className="buttons-container">
+            <Form.Group>
+              <Button onClick={submitForm}>Log In</Button>
+            </Form.Group>
+            <Form.Group>
+              <Link className="register-link" to="/register">
+                Register
+              </Link>
+            </Form.Group>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
